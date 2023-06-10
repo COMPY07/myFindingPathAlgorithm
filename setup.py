38c63e9eb441452c1770536f3527afc31442b981
JeongHun Lee <56685470+COMPY07@users.noreply.github.com>
2023-06-10 22:06:57 +0900

    setupFile upload
    setup 파일 업로드 및 pygame 설치 설정

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="CompyLib",
+    version="0.1-Prototype",
+    description="compy pip install compy",
+    url="https://github.com/COMPY07/myFindingPathAlgorithm.git",
+    author="JeongHun Lee",
+    license="JeongHun Lee(Compy)",
+    packages=['CompyLib'],
+    zip_safe=False,
+    install_requires=['pygame==2.4.0']
+)
