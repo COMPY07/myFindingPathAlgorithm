4c5e835789598b07c998d008685af351fc6d7ee9
JeongHun Lee <56685470+COMPY07@users.noreply.github.com>
2023-06-17 18:43:23 +0900

    Update AStar.py
    Change Astar

--- CompyLib/Algorithms/AStar.py
+++ CompyLib/Algorithms/AStar.py
@@ -9,12 +9,13 @@ class Astar():
         return D1 * (dx + dy) + (D2 - 2 * D1) * min(dx, dy)
 
     def heuristic_Euclidean_Distance(self, current, end):
-        return ((current.position[0] - end.position[0]) ** 2) + ((current.position[1] - end.position[1]) ** 2)
+        return ((current.position[0] - end.position[0]) ** 2)\
+                + ((current.position[1] - end.position[1]) ** 2)
 
-    def CalcNode(self, currentNode, endNode):
-        currentNode.g = currentNode.g + 1
-        currentNode.h = self.heuristic_Euclidean_Distance(currentNode, endNode)
-        currentNode.f = currentNode.g + currentNode.h + currentNode.density ** 2
+    def CalcNode(self, childNode, currentNode, endNode):
+        childNode.g = currentNode.g + 1 + childNode.density ** 2
+        childNode.h = self.heuristic_Euclidean_Distance(currentNode, endNode)
+        childNode.f = childNode.g + childNode.h
 
     def NodeSet(self, node, g: int, f: int, h: int):
         node.g = g
@@ -23,7 +24,7 @@ class Astar():
 
     def getPath(self, node, board: list[list]):
         path = []
-        while node != None:
+        while node.parent != None:
             y, x = node.position
             board[y][x] = node.f
             path.append(node.position)
@@ -32,10 +33,10 @@ class Astar():
 
     def AStar(self, board: list[list], start: tuple, end: tuple, delay: int = 0):
         import heapq as hq
-        from Node import Node
+        from CompyLib.Algorithms import Node
 
-        startNode = board[start[0]][start[1]]
-        endNode = board[end[0]][end[1]]
+        startNode = Node(None, start, board[start[0]][start[1]], 0)
+        endNode = Node(None, end, board[end[0]][end[1]], 0)
 
         self.NodeSet(startNode, 0, 0, 0)
 
@@ -46,36 +47,44 @@ class Astar():
         dx, dy = [0, 0, -1, 1, -1, -1, 1, 1], [-1, 1, 0, 0, -1, 1, -1, 1]
 
         Visualization = self.grid != None
-
+        self.CalcNode(startNode, startNode, endNode)
         while openList:
             currentNode: Node = hq.heappop(openList)
             closedList.append(currentNode)
-
             if Visualization:
                 self.grid.changeBox(self.grid.getBoxbyIndex(currentNode.position[0],
-                                                  currentNode.position[1]), "GREEN")
+                                                            currentNode.position[1]), "GREEN")
 
             if endNode == currentNode:
                 return self.getPath(currentNode, board)
 
-            # children: list[Node] = []
+            children: list[Node] = []
 
             for i in range(8):
                 ny = currentNode.position[0] + dy[i]
                 nx = currentNode.position[1] + dx[i]
 
-                if 0 > ny >= len(board) or 0 > nx >= len(board[0]) or board[ny][nx].state == 1:
+                if 0 > ny or ny >= len(board) or 0 > nx or \
+                        nx >= len(board[0]) \
+                        or board[ny][nx] == 1:
                     continue
+                children.append(Node(None, (ny, nx), board[ny][nx], 0))
+            for child in children:
+                if child in closedList: continue
+                self.CalcNode(child, currentNode, endNode)
+                # print(child.f, currentNode.f, currentNode, child)
+                # if child.f > currentNode.f: continue or child.g + child.density > currentNode.g + 1: continue  # or
+                # child.g + child.density <= currentNode.g + 1:
+                flag: bool = False
+                for node in openList:
+                    if child == node and child.g > node.g:
+                        flag = True
+                        break
+                if flag: continue
 
-                # children.append(board[ny][nx])
-
-                child: Node = board[ny][nx]
-
-                if child in closedList or child.g + child.density <= currentNode.g + 1:
-                    continue
-                self.CalcNode(child, endNode)
                 child.parent = currentNode
 
                 hq.heappush(openList, child)
             if Visualization: self.grid.boardUpdate(delay)
+
         return -1
